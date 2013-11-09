4ed21f66c0f265e086884919b5f14909dd8e55d7
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2013-11-09 13:00:24 +0200

    Refine light_leds.s
    Refine light_leds.s by using unified syntax, ldr instructions instead
    of explicit mov*, and removing '#' prefix from immediate operands.

--- light_leds.s
+++ light_leds.s
@@ -1,3 +1,5 @@
+.syntax unified
+
 gpio1_base          = 0x4804c000
 gpio_oe             = 0x134
 gpio_cleardataout   = 0x190
@@ -6,13 +8,12 @@ gpio_setdataout     = 0x194
 .text
 .global start
 start:
-    movw r1, (gpio1_base & 0xffff)
-    movt r1, (gpio1_base >> 16)
-    mov r2, #0xfe1fffff
-    str r2, [r1, #gpio_oe]
+    ldr r1, =gpio1_base
+    ldr r2, =0xfe1fffff
+    str r2, [r1, gpio_oe]
     mvn r2, r2
-    str r2, [r1, #gpio_cleardataout]
-    str r2, [r1, #gpio_setdataout]
+    str r2, [r1, gpio_cleardataout]
+    str r2, [r1, gpio_setdataout]
 loop:
     wfe
     b loop

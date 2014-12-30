09f1e57011375f3fbd4d4a4327c8b4506d4f8c4d
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2014-12-30 21:10:33 +0200

    Improve debug.* comments a bit

--- debug.h
+++ debug.h
@@ -8,9 +8,14 @@
 
 /* Dump core register to UART0 */
 .macro debug_dump_core_regs
+    /* Store all registers on the stack */
     stmfd sp, {r0-r15}
     sub sp, sp, 16 * 4
+
+    /* Output stored registers + CPSR */
     bl _debug_dump_core_regs
+
+    /* Restore all registers except PC */
     pop {r0-r12}
     add sp, sp, 4   /* Skip SP */
     pop {lr}
